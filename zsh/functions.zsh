e6cb354512fddd8aca037e2a5de81cb06136887d
obxhdx <obxhdx@yahoo.com>
2017-06-08 17:27:40 -0300

    Add Codi helper function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -135,3 +135,16 @@ eval "$(cat <<EOF
 EOF
 )"
 }
+
+# Codi
+# Usage: codi [filetype] [filename]
+codi() {
+  local syntax="${1:-python}"
+  shift
+  vim -c \
+    "set bt=nofile ls=0 noru nonu nornu |\
+    hi ColorColumn ctermbg=NONE |\
+    hi VertSplit ctermbg=NONE |\
+    hi NonText ctermfg=0 |\
+    Codi $syntax" "$@"
+}
