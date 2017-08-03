1914f41bdc43733eb022cefc24d85c3716e27232
obxhdx <obxhdx@yahoo.com>
2017-08-02 22:55:33 -0300

    Remove some history options

--- bash/base.bash
+++ bash/base.bash
@@ -6,8 +6,6 @@ complete -d cd # cd tab completion only completes directories
 HISTFILESIZE=1000000 # allow a larger history file
 HISTSIZE=1000000 # allow a larger history file
 HISTCONTROL=ignoreboth:erasedups
-HISTIGNORE='l:ls:bg:fg:cd:history' # commands to ignore
-HISTTIMEFORMAT='%F %T ' # record timestamps
 PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r" # preserve bash history in multiple terminal windows
 shopt -s histappend # append history instead of rewriting it
 shopt -s cmdhist # use one command per line
