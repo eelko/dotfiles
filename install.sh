12f60966d52daabf0c1ec3de20930d782187f119
obxhdx <obxhdx@yahoo.com>
2020-01-26 22:10:34 -0800

    misc: fix shellcheck errors on install.sh

--- install.sh
+++ install.sh
@@ -13,10 +13,14 @@ DOTFILES_REPO="https://github.com/obxhdx/dotfiles"
 DOTFILES_HOME="$HOME/.dotfiles"
 
 # clone repo
-[ -d "$DOTFILES_HOME" ] && echo "Directory $DOTFILES_HOME already exists" || git clone --recursive "$DOTFILES_REPO" "$DOTFILES_HOME"
+if [[ -d "$DOTFILES_HOME" ]]; then
+  echo "Directory $DOTFILES_HOME already exists"
+else
+  git clone --recursive "$DOTFILES_REPO" "$DOTFILES_HOME"
+fi
 
 # symlink dotfiles
-find $DOTFILES_HOME/config/* -maxdepth 0 -exec bash -c 'ln -snv {} ~/.$(basename {})' \;
+find "$DOTFILES_HOME/config/*" -maxdepth 0 -exec bash -c 'ln -snv $1 ~/.$(basename $1)' _ {} \;
 
 # install tmux plugins
 ~/.tmux/plugins/tpm/bin/install_plugins
