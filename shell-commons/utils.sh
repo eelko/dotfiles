c10db2a9639c3697c1fb875c98627d213b556c55
obxhdx <obxhdx@yahoo.com>
2020-08-03 16:19:27 -0700

    tmux: truncate window names when too big

--- shell-commons/utils.sh
+++ shell-commons/utils.sh
@@ -5,7 +5,14 @@ update_tmux_pane_name() {
   [ -n "$TMUX" ] || return
 
   local -r current_path=$(basename "$(tmux display-message -p '#{pane_current_path}')")
-  local -r current_dir_name=$(echo "$current_path" | awk -F- '{if (NF>1) {print $(NF-1)"-"$NF} else {print $NF}}') # only last two words of kebab-cased names
+
+  if [[ $current_path =~ - ]]; then
+    # only last two words of kebab-cased names
+    current_dir_name=$(echo "$current_path" | awk -F- '{if (length($0) > 15) {print $(NF-1)"-"$NF} else {print $0}}')
+  else
+    # truncate at 16 characters
+    current_dir_name=${current_path:0:15}
+  fi
 
   if [[ -n "$1" ]]; then
     # called by preexec zsh hook; $1 has entire command with arguments
