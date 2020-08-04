377ebb6a3558df78c338970d591530d8dc3bd530
obxhdx <obxhdx@yahoo.com>
2020-08-03 21:06:11 -0700

    vim: include nvim python client in update script

--- update.sh
+++ update.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Update submodules
 git submodule foreach git pull origin master
 git pull --rebase
-$EDITOR +PlugUpgrade +PlugUpdate +'call LoadPlugins()' +CocUpdateSync +qall
+
+# Update Python client to Neovim
+which pip2 >/dev/null && pip2 install --upgrade pynvim
+which pip3 >/dev/null && pip3 install --upgrade pynvim
+
+# Update Neovim plugins
+$EDITOR +PlugUpgrade +PlugUpdate +CocUpdateSync +qall
