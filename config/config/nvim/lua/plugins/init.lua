2dae70035edddaa8d9d0edfa67baada0a79e3112
obxhdx <obxhdx@yahoo.com>
2021-10-22 22:41:29 -0700

    vim: replace mhinz/vim-signify with lewis6991/gitsigns.nvim

--- config/config/nvim/lua/plugins/init.lua
+++ config/config/nvim/lua/plugins/init.lua
@@ -58,20 +58,34 @@ return packer.startup(function(use)
 
   -- Git sings on gutter
   use {
-    'mhinz/vim-signify',
+    'lewis6991/gitsigns.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim',
+    },
     config = function()
-      vim.g.signify_sign_show_count = false
-      vim.g.signify_priority = 5
-      vim.g.signify_sign_add = '│'
-      vim.g.signify_sign_delete = '│'
-      vim.g.signify_sign_delete_first_line = '│'
-      vim.g.signify_sign_change = '│'
-      vim.g.signify_sign_changedelete = '│'
-      cmd [[
-        hi SignifySignAdd    guifg=lightgreen guibg=NONE
-        hi SignifySignChange guifg=cyan guibg=NONE
-        hi SignifySignDelete guifg=lightred guibg=NONE
-      ]]
+      require('gitsigns').setup {
+        signs = {
+          add = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+          change = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+          delete = { hl = 'GitSignsDelete', text = '│', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+          topdelete = { hl = 'GitSignsDelete', text = '│', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+          changedelete = {
+            hl = 'GitSignsChange',
+            text = '│',
+            numhl = 'GitSignsChangeNr',
+            linehl = 'GitSignsChangeLn',
+          },
+        },
+        keymaps = {
+          -- Default keymap options
+          noremap = true,
+          ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'" },
+          ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'" },
+          -- Text objects
+          ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+          ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+        },
+      }
     end,
   }
 
