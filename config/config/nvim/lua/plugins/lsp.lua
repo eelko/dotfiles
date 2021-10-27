7de4a85de66e80974c1cf7367b29bf30491258a2
obxhdx <obxhdx@yahoo.com>
2021-10-26 18:56:37 -0700

    vim: remove redundant lsp config

--- config/config/nvim/lua/plugins/lsp.lua
+++ config/config/nvim/lua/plugins/lsp.lua
@@ -191,7 +191,6 @@ local on_attach = function(client)
 end
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local flags = {
   debounce_text_changes = 150,
