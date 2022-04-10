9a74be49bc6bd4807bbf96465ae4da76c1956eaa
obxhdx <obxhdx@yahoo.com>
2022-04-10 15:51:35 -0700

    vim: fancier git gutter signs

--- config/config/nvim/lua/plugins/init.lua
+++ config/config/nvim/lua/plugins/init.lua
@@ -71,13 +71,13 @@ return packer.startup(function(use)
     config = function()
       require('gitsigns').setup {
         signs = {
-          add = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-          change = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-          delete = { hl = 'GitSignsDelete', text = '│', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-          topdelete = { hl = 'GitSignsDelete', text = '│', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+          add = { hl = 'GitSignsAdd', text = '▎', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+          change = { hl = 'GitSignsChange', text = '▎', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+          delete = { hl = 'GitSignsDelete', text = '▎', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+          topdelete = { hl = 'GitSignsDelete', text = '▎', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
           changedelete = {
             hl = 'GitSignsChange',
-            text = '│',
+            text = '▎',
             numhl = 'GitSignsChangeNr',
             linehl = 'GitSignsChangeLn',
           },
