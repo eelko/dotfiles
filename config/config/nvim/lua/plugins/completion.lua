6bc202f8d342ee581d9ffb39f566fe5857c42824
obxhdx <obxhdx@yahoo.com>
2022-05-20 11:03:22 -0700

    vim: remove italic from cmp ghost text

--- config/config/nvim/lua/plugins/completion.lua
+++ config/config/nvim/lua/plugins/completion.lua
@@ -1,3 +1,7 @@
+require 'utils'
+
+highlight('CmpGhostText', { fg = '#565f89' })
+
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
@@ -43,7 +47,7 @@ cmp.setup {
   },
 
   experimental = {
-    ghost_text = true,
+    ghost_text = { hl_group = 'CmpGhostText' },
   },
 
   formatting = {
