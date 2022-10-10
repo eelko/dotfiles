70f5ed10117e3d67ea4c9e7b6502c55b10726a3c
obxhdx <obxhdx@yahoo.com>
2022-10-10 12:43:57 -0700

    vim: define color for CurSearch (neovim 0.8)

--- config/config/nvim/lua/plugins/init.lua
+++ config/config/nvim/lua/plugins/init.lua
@@ -76,6 +76,7 @@ return require('packer').startup {
       event = 'UIEnter',
       config = function()
         vim.cmd 'color tokyonight'
+        highlight('CurSearch', { bg = '#637ab3', fg = '#c0caf5' })
         highlight('Folded', { bg = '#24283b', fg = '#565f89' })
       end,
     }
