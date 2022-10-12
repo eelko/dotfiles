456af4484bf201fd1af593052a5304cf8a4e17a9
obxhdx <obxhdx@yahoo.com>
2022-10-12 13:17:24 -0700

    vim: minor statusline tweaks/cleanup

--- config/config/nvim/lua/plugins/statusline_basic.lua
+++ config/config/nvim/lua/plugins/statusline_basic.lua
@@ -2,7 +2,10 @@ require 'utils'
 
 local config = require 'tokyonight.config'
 local colors = require('tokyonight.colors').setup(config)
-local custom_theme = require 'lualine.themes.tokyonight'
+local theme = require 'lualine.themes.tokyonight'
+
+local bg = theme.normal.b.bg
+local fg = theme.normal.c.fg
 
 require('lualine').setup {
   options = {
@@ -10,41 +13,28 @@ require('lualine').setup {
     section_separators = { left = '', right = '' },
     theme = {
       normal = {
-        a = { bg = '#303650', fg = '#a9b1d6' },
-        b = { bg = '#303650', fg = '#a9b1d6' },
-        c = { bg = '#303650', fg = '#a9b1d6' },
-      },
-      inactive = {
-        a = { bg = '#1f2335', fg = '#a9b1d6' },
-        b = { bg = '#1f2335', fg = '#a9b1d6' },
-        c = { bg = '#1f2335', fg = '#a9b1d6' },
+        a = { bg = bg, fg = fg },
+        b = { bg = bg, fg = fg },
+        c = { bg = bg, fg = fg },
       },
     },
   },
   sections = {
     lualine_a = { { 'mode', color = { gui = 'bold' } } },
     lualine_b = {
-      { 'branch', icon = '', padding = { left = 2 } },
-      {
-        'diff',
-        diff_color = {
-          added = { fg = colors.git.add },
-          modified = { fg = colors.git.change },
-          removed = { fg = colors.git.delete },
-        },
-        symbols = { added = ' ', modified = '柳', removed = ' ' },
-      },
+      { 'branch', icon = '', padding = { left = 3 } },
+      { 'diff' },
     },
     lualine_c = {
-      { 'filetype', colored = false, icon_only = true, padding = { left = 2 } },
+      { 'filetype', colored = false, icon_only = true, padding = { left = 3 } },
       {
         'filename',
-        symbols = { modified = '  ', readonly = '  ' },
+        symbols = { modified = ' ', readonly = ' ' },
         cond = function()
-          return not contains({ 'NvimTree', 'neo-tree', 'TelescopePrompt' }, vim.o.filetype)
+          return not contains({ 'NvimTree', 'TelescopePrompt' }, vim.o.filetype)
         end,
       },
-      { 'diagnostics', sources = { 'nvim_diagnostic', 'coc' } },
+      { 'diagnostics', icon = ' ', sources = { 'nvim_diagnostic' }, padding = { left = 3 } },
     },
     lualine_x = {},
     lualine_y = { 'progress' },
