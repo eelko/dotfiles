900342bfc6d115eafd592c7c03a5187151adfda7
obxhdx <obxhdx@yahoo.com>
2022-11-08 16:46:51 -0800

    vim: tweak modes.nvim to make current line number bold

--- config/config/nvim/lua/plugins/init.lua
+++ config/config/nvim/lua/plugins/init.lua
@@ -800,10 +800,10 @@ return require('packer').startup {
         require('modes').setup()
 
         -- Highlight numbers fg only
-        highlight('ModesCopyCursorLineNr', { fg = '#f5c359' })
-        highlight('ModesInsertCursorLineNr', { fg = '#78ccc5' })
-        highlight('ModesDeleteCursorLineNr', { fg = '#c75c6a' })
-        highlight('ModesVisualCursorLineNr', { fg = '#9745be' })
+        highlight('ModesCopyCursorLineNr', { fg = '#f5c359', bold = true })
+        highlight('ModesInsertCursorLineNr', { fg = '#78ccc5', bold = true })
+        highlight('ModesDeleteCursorLineNr', { fg = '#c75c6a', bold = true })
+        highlight('ModesVisualCursorLineNr', { fg = '#9745be', bold = true })
 
         -- Don't highlight sign column
         highlight('ModesCopyCursorLineSign', { bg = 'none' })
