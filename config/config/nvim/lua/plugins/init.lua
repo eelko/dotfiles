4771660cd64d4f8502288feacb688397eef07e14
obxhdx <obxhdx@yahoo.com>
2022-11-16 21:04:58 -0800

    vim: icon updates

--- config/config/nvim/lua/plugins/init.lua
+++ config/config/nvim/lua/plugins/init.lua
@@ -109,15 +109,41 @@ return require('packer').startup {
 
         -- override icons but reuse original colors
         local _, js_color = devicons.get_icon_color('foo.js', 'js')
+        local _, md_color = devicons.get_icon_color('foo.md', 'markdown')
         local _, ts_color = devicons.get_icon_color('foo.ts', 'ts')
 
-        require('nvim-web-devicons').set_icon {
-          js = {
+        devicons.set_icon {
+          ['js'] = {
             icon = '',
             color = js_color,
             name = 'Js',
           },
-          ts = {
+          ['md'] = {
+            icon = '',
+            color = md_color,
+            name = 'Md',
+          },
+          ['test.js'] = {
+            icon = '',
+            color = js_color,
+            name = 'TestJavascript',
+          },
+          ['test.jsx'] = {
+            icon = '',
+            color = js_color,
+            name = 'TestJavascriptReact',
+          },
+          ['test.ts'] = {
+            icon = '',
+            color = ts_color,
+            name = 'TestTypescript',
+          },
+          ['test.tsx'] = {
+            icon = '',
+            color = ts_color,
+            name = 'TestTypescriptReact',
+          },
+          ['ts'] = {
             icon = 'ﯤ',
             color = ts_color,
             name = 'Ts',
