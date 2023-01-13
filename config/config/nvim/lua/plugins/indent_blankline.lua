c46e1bc02a72a7a24206a9ac28b5ab0b95586b18
obxhdx <obxhdx@yahoo.com>
2023-01-13 10:37:07 -0800

    vim: work around indent guide first level not being disabled correctly

--- config/config/nvim/lua/plugins/indent_blankline.lua
+++ config/config/nvim/lua/plugins/indent_blankline.lua
@@ -1,6 +1,6 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
-  event = 'UIEnter',
+  event = 'BufReadPost', -- cannot be UIEnter or `show_first_indent_level=false` won't work until cursor moves
   config = function()
     require('indent_blankline').setup {
       show_current_context = true,
