2165951798fd17f4551502d02b5a698e06119e80
obxhdx <obxhdx@yahoo.com>
2023-01-27 11:47:35 -0800

    vim: disable leap.nvim `x` mapping

--- config/config/nvim/lua/plugins/leap.lua
+++ config/config/nvim/lua/plugins/leap.lua
@@ -4,6 +4,9 @@ return {
   event = 'VeryLazy',
   config = function()
     require('leap').add_default_mappings()
+    vim.keymap.del({ 'x', 'o' }, 'x')
+    vim.keymap.del({ 'x', 'o' }, 'X')
+
     require('leap-spooky').setup {}
   end,
 }
