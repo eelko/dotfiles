6a29c97ebe90b8117b3f49c88cea4ce602b6d884
obxhdx <obxhdx@yahoo.com>
2023-03-07 14:01:06 -0800

    vim: remove unused plugins

--- config/config/nvim/lua/plugins/bg.lua
+++
@@ -1,4 +0,0 @@
-return {
-  'typicode/bg.nvim',
-  event = 'ColorScheme',
-}
--- config/config/nvim/lua/plugins/leap.lua
+++ config/config/nvim/lua/plugins/leap.lua
@@ -1,12 +1,9 @@
 return {
   'ggandor/leap.nvim',
-  dependencies = 'ggandor/leap-spooky.nvim',
   event = 'VeryLazy',
   config = function()
     require('leap').add_default_mappings()
     vim.keymap.del({ 'x', 'o' }, 'x')
     vim.keymap.del({ 'x', 'o' }, 'X')
-
-    require('leap-spooky').setup {}
   end,
 }
