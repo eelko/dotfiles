ce4575b2f25fa26c0b40403396d53ff6b485f5df
obxhdx <obxhdx@yahoo.com>
2023-04-10 11:35:50 -0700

    vim: enable Hop on visual mode

--- config/config/nvim/lua/plugins/hop.lua
+++ config/config/nvim/lua/plugins/hop.lua
@@ -2,8 +2,14 @@ return {
   'phaazon/hop.nvim',
   branch = 'v2',
   keys = {
-    { 's', ':HopChar2<CR>', desc = '[Hop] Go to any bigram in the current buffer', silent = true },
-    { 'gs', ':HopChar2MW<CR>', desc = '[Hop] Go to any bigram in any visible buffer', silent = true },
+    {
+      's',
+      '<cmd>HopChar2<CR>',
+      mode = { 'n', 'v' },
+      desc = '[Hop] Go to any bigram in the current buffer',
+      silent = true,
+    },
+    { 'gs', '<cmd>HopChar2MW<CR>', desc = '[Hop] Go to any bigram in any visible buffer', silent = true },
   },
   config = function()
     require('hop').setup {}
