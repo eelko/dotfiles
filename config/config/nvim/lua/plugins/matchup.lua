e30d072a5e4068ef5a900cec6644b33df4a61827
obxhdx <obxhdx@yahoo.com>
2023-04-12 23:26:16 -0700

    vim: load matchup on bufread

--- config/config/nvim/lua/plugins/matchup.lua
+++ config/config/nvim/lua/plugins/matchup.lua
@@ -1,6 +1,6 @@
 return {
   'andymass/vim-matchup',
-  event = 'VeryLazy',
+  event = 'BufReadPost',
   config = function()
     vim.g.matchup_matchparen_deferred = true
     vim.g.matchup_matchparen_hi_surround_always = true
