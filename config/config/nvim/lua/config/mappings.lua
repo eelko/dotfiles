9291b1a187b1978dca83f3d76b153ce26ac795e6
obxhdx <obxhdx@yahoo.com>
2023-04-14 11:57:11 -0700

    vim: enhance gn/gp mappings

--- config/config/nvim/lua/config/mappings.lua
+++ config/config/nvim/lua/config/mappings.lua
@@ -9,17 +9,9 @@ map('c', '<cn>', '<down>', { desc = 'Go to next entry in command history' })
 map('c', '<cp>', '<up>', { desc = 'Go to previous entry in command history' })
 
 -- Normal mode
-map('n', 'gn', ':if &buftype == "" | bnext | endif<CR>', { desc = 'Go to next buffer' })
-map('n', 'gp', ':if &buftype == "" | bprevious | endif<CR>', { desc = 'Go to previous buffer' })
 map('n', '<c-w>tc', ':tabclose<cr>', { desc = 'Close current tab' })
 map('n', '<c-w>tn', ':tabnew %<cr>', { desc = 'Create new tab' })
 map('n', '<esc>', ':nohlsearch<cr>', { desc = 'Clear highlighted search matches' })
-map(
-  'n',
-  '<leader>x',
-  "(bufnr('%') == getbufinfo({'buflisted': 1})[-1].bufnr ? ':bp' : ':bn').'<bar>bd #<CR>'",
-  { desc = 'Save and cleverly close current buffer (based on reddit.com/em9qvv)', expr = true }
-)
 map('n', '<leader>Q', ':qall!<cr>', { desc = 'Quit editor ignoring unsaved buffers' })
 map('n', '<leader>q', ':qall<cr>', { desc = 'Quit editor' })
 map('n', '<leader>w', ':write<cr>', { desc = 'Save current buffer' })
@@ -30,6 +22,16 @@ map('n', ']q', ':cnext<CR>', { desc = 'Go to next quickfix entry' })
 map('n', 'gy', '`[v`]', { desc = 'Visual select last yank/paste' })
 map('n', 'j', 'gj', { desc = 'Navigate down wrapped lines' })
 map('n', 'k', 'gk', { desc = 'Navigate up wrapped lines' })
+
+map('n', '<leader>x', require('utils').clever_close, { desc = 'Save and close current buffer' })
+
+map('n', 'gn', function()
+  require('utils').switch_buffer 'next'
+end, { desc = 'Go to next buffer' })
+
+map('n', 'gp', function()
+  require('utils').switch_buffer 'previous'
+end, { desc = 'Go to previous buffer' })
 
 -- Visual mode
 map('x', 'p', 'pgvy', { desc = 'Paste in visual mode without yanking' })
--- config/config/nvim/lua/utils.lua
+++ config/config/nvim/lua/utils.lua
@@ -33,4 +33,75 @@ function M.map(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, options)
 end
 
+--- Closes the current buffer in a "clever" way.
+--
+-- This function closes the current buffer and switches to the next buffer or
+-- the previous one, depending on its position in the buffer list. If the
+-- current buffer is the last listed buffer, it switches to the previous buffer
+-- (':bp'). Otherwise, it switches to the next buffer (':bn').
+--
+-- This behavior is based on the original Vimscript mapping found at
+-- reddit.com/em9qvv.
+function M.clever_close()
+  -- Get the current buffer number
+  local current_buf = vim.api.nvim_get_current_buf()
+
+  -- Get the list of all buffer numbers
+  local listed_buffers = vim.api.nvim_list_bufs()
+
+  -- Initialize the last listed buffer variable
+  local last_listed_buf = nil
+
+  -- Iterate through the buffer list to find the last listed buffer
+  for _, buf in ipairs(listed_buffers) do
+    if vim.api.nvim_buf_get_option(buf, 'buflisted') then
+      last_listed_buf = buf
+    end
+  end
+
+  -- If the current buffer is the last listed buffer, switch to the previous buffer
+  if current_buf == last_listed_buf then
+    vim.cmd 'bp'
+    -- Otherwise, switch to the next buffer
+  else
+    vim.cmd 'bn'
+  end
+
+  -- Delete the buffer that was originally current
+  vim.cmd 'bd #'
+end
+
+--- Switches to the next or previous buffer based on the given direction.
+-- This function switches the buffer `count` times in the specified direction ('next' or 'previous').
+-- It sets a mark in the current buffer before switching, allowing you to return to the original buffer
+-- with <C-o>. It does nothing if the current buffer's `buftype` is not empty.
+-- @param direction string: 'next' or 'previous', indicating the direction to switch the buffer
+function M.switch_buffer(direction)
+  if vim.bo.buftype ~= '' then
+    return
+  end
+
+  local count = vim.v.count1 > 0 and vim.v.count1 or 1
+  local bufnr = vim.api.nvim_get_current_buf()
+  local total_buffers = vim.fn.bufnr '$'
+
+  -- Set a mark in the current buffer
+  vim.cmd "normal! m'"
+
+  for _ = 1, count do
+    repeat
+      if direction == 'next' then
+        bufnr = bufnr % total_buffers + 1
+      else
+        bufnr = bufnr - 1
+        if bufnr < 1 then
+          bufnr = total_buffers
+        end
+      end
+    until vim.fn.buflisted(bufnr) == 1
+  end
+
+  vim.cmd('keepjumps ' .. 'buf ' .. bufnr)
+end
+
 return M
